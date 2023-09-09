0508fc749ddc623059479a35d1edc754351b5727
nbanuri <nikhil.tollplus@outlook.com>
2023-09-09 09:29:01 +0530

    Lazy Loading
    Section 9: Lazy Loading with Modules
    119. Building a Reusable Table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
