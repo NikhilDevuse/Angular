9b99799be52f9dd188d703d891a53819e696de7a
Nikhil <nikhilbanuri4@gmail.com>
2023-10-11 07:39:13 +0530

    Advanced Component routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    CollectionsModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/elements/times.directive.ts
+++ src/app/elements/times.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTimes]'
 })
 export class TimesDirective {
-
-  constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<any>) { }
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) {}
 
   @Input('appTimes') set render(times: number) {
     this.viewContainer.clear();
@@ -14,5 +16,4 @@ export class TimesDirective {
       this.viewContainer.createEmbeddedView(this.templateRef, {});
     }
   }
-
 }
